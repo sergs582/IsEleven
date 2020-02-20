b5dac34ed5d157ce06209a13717ba67a44638a74
sergs582 <kurba37@gmail.com>
2020-02-20 12:54:28 +0300

    init commit

---
+++ IsEleven/IsEleven.h
@@ -0,0 +1,19 @@
+//
+//  IsEleven.h
+//  IsEleven
+//
+//  Created by Сергей on 20.02.2020.
+//  Copyright © 2020 Сергей. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for IsEleven.
+FOUNDATION_EXPORT double IsElevenVersionNumber;
+
+//! Project version string for IsEleven.
+FOUNDATION_EXPORT const unsigned char IsElevenVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IsEleven/PublicHeader.h>
+
+
